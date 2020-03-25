bbdb72829a7eb54c54f2c1673b6e6c4a28703bc4
Cecilio Cannav <cecilio.cannav@gmail.com>
2020-03-25 12:57:26 +0100

    T

--- setup.py
+++ setup.py
@@ -1,17 +1,12 @@
-from setuptools import setup, find_packages
-from distutils.core import setup
+import setuptools
 
-with open('README.md') as readme_file:
-    README = readme_file.read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-#with open('HISTORY.md') as history_file:
-#    HISTORY = history_file.read()
-
-
-setup(
+setuptools.setup(
   name = 'smktest',
   packages = ['smktest'], # this must be the same as the name above
-  version = '0.1.3',
+  version = '0.1.4',
   description = 'This package offers a series of tests to create the correct Smoke Test of an application before uploading changes to production.',
   author = 'Cecilio Cannavacciuolo Diaz',
   author_email = 'cecilio.cannav@gmail.com',
@@ -20,17 +15,6 @@ setup(
   keywords = ['smoketest', 'testing', 'python', 'smart',],
   python_requires='>=3',
 )
-
-#setup(name='smktest',
-#      version='0.1',
-#      description='Easy way to make markdown code for tables',
-#      url='https://github.com/cecilio-cannav/smktest.git',
-#      author='cecilio cannavacciuolo diaz',
-#      author_email='cecilio.cannav@gmail.com',
-#      license='MIT',
-#      packages=['smktest'],
-#      )
-
 
 install_requires = [
     'subprocess',
